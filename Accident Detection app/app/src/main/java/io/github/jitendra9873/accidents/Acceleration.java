01533ee4060908e7547deb84d8a01d43fd72d874
jitendra9873 <jitendrakumhar1@gmail.com>
2018-07-01 07:06:00 +0530

    first commit and recording in wav format

---
+++ Accident Detection app/app/src/main/java/io/github/jitendra9873/accidents/Acceleration.java
@@ -0,0 +1,32 @@
+package io.github.jitendra9873.accidents;
+
+public class Acceleration {
+    private long timestamp;
+    private double x;
+    private double y;
+    private double z;
+
+    public Acceleration(float x_value, float y_value, float z_value, long timestamp) {
+        x= new Double(""+x_value);
+        y= new Double(""+y_value);
+        z= new Double(""+z_value);
+        this.timestamp = timestamp;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+}
+
